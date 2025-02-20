5ee512936e595814c4ee576e37fc2bd0dc8b80ae
Frnzdev <felipeacfornazeiro@gmail.com>
2025-02-20 12:18:51 -0300

    chore: tem coisa para arrumar

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -39,21 +39,6 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
           <CardContent className="py-08 flex flex-col items-center gap-8">
             <div className="relative h-[80px] w-[80px]">
               <Image
-                src="/Dine_in.png"
-                fill
-                alt="Para comer aqui"
-                className="object-contain"
-              ></Image>
-            </div>
-            <Button variant="secondary" className="rounded-full">
-              Para comer aqui
-            </Button>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent className="py-08 flex flex-col items-center gap-8">
-            <div className="relative h-[80px] w-[80px]">
-              <Image
                 src="/Take_away.png"
                 fill
                 alt="Para levar"
